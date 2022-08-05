0f468c5f38494b4006cb4cadb7511fcc9bb3edf8
StevenZhou90 <70081222+StevenZhou90@users.noreply.github.com>
2022-08-05 02:42:37 -0600

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-from dataset_builder.subset_tool import subset_class #To be imported
-from dataset_builder.data_splitter import filter_class 
+from dataset_builder.subset_tool import SubsetClass
+from dataset_builder.data_splitter_class import FilterClass 
 
 '''Add the path to your attribute dataset here: Make sure it is a npy file'''
 attrARR_path = './data/attrArr1000.npy'
@@ -8,19 +8,14 @@ paths_path = './data/paths1000.npy'
 '''Add the path to a csv with all attribute column names'''
 columns = './data/columns.csv'
 
-filter_tool = filter_class(attrARR_path, paths_path, columns)
+filter_tool = FilterClass(attrARR_path, paths_path, columns)
 
 '''Examples of tuple inputs'''
-tup1 = ('Heavy_Makeup', 'abs', 0, 100)
-tup2 = ('vitor_gender', 0)
-tup_list = [tup1, tup2]
-
-'''example on how tuples will be added'''
-for tup in tup_list:
-  filter_tool.add_mask(tup)
-  
-new_paths = filter_tool.make_new_paths()
-
-subset_tool = SubsetClass(new_paths, 3, 3, 3, 'test_file_name', False)
-
+# tup1 = ('Heavy_Makeup', 'abs', 0, 100)
+# tup2 = ('vitor_gender', 0)
+# tup_list = [tup1, tup2]
+tup_list = []
+new_paths = filter_tool.run(tup_list)
+print(new_paths.shape)
+subset_tool = SubsetClass(new_paths, 10, 10, 10, 'test_file_name', False, tup_list)
 subset_tool.write_val_sets()
