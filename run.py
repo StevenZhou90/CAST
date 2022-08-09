31a17a07c67b788ff61f6a06826596b8be6ee65d
StevenZhou90 <70081222+StevenZhou90@users.noreply.github.com>
2022-08-09 17:42:45 -0600

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from dataset_builder.subset_tool import SubsetClass
-from dataset_builder.dataset_splitter import FilterClass 
+from dataset_builder.data_splitter_class import FilterClass 
 import os
 
 '''Add the path to your attribute dataset here: Make sure it is a npy file'''
@@ -11,14 +11,18 @@ paths_path = './data/paths1000.npy'
 '''Add the path to a csv with all attribute column names'''
 columns = './data/columns.csv'
 
-filter_tool = FilterClass(attrARR_path, paths_path, columns)
+'''OPTIONAL: Add indexes of training samples'''
+train_set = 'WebFace12M'
+
+filter_tool = FilterClass(attrARR_path, paths_path, columns, train_set)
 
 '''Examples of tuple inputs'''
-tup1 = ('Heavy_Makeup', 'abs', 0, 100)
-tup2 = ('vitor_gender', 0)
-tup_list = [tup1, tup2]
+attr1 = ('Heavy_Makeup', 'abs', 0, 100)
+attr2 = ('vitor_gender', 0)
+attr_list = [attr1, attr2]
 
-new_paths = filter_tool.run(tup_list)
+new_paths = filter_tool.run(attr_list)
+print(len(new_paths))
 
-subset_tool = SubsetClass(new_paths, 10, 10000, 10000, 'heavyMakeupAndGender', False, tup_list)
+subset_tool = SubsetClass(new_paths, 10, 10, 10, 'loser face', False, attr_list)
 subset_tool.write_val_sets()
