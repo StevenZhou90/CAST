2f7db8ce8b4fb5f180440b3d6de280e7e8253757
StevenZhou90 <70081222+StevenZhou90@users.noreply.github.com>
2022-08-16 00:00:15 -0600

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-from dataset_builder.subset_tool import SubsetClass
 from dataset_builder.data_splitter_class import FilterClass 
-import os
 
 '''Add the path to your attribute dataset here: Make sure it is a npy file'''
 attrARR_path = './data/attrArr1000.npy'
@@ -9,20 +7,22 @@ attrARR_path = './data/attrArr1000.npy'
 paths_path = './data/paths1000.npy'
 
 '''Add the path to a csv with all attribute column names'''
-columns = './data/columns.csv'
+columns_path = './data/columns.csv'
 
 '''OPTIONAL: Add indexes of training samples'''
-train_set = 'WebFace12M'
+train_set_path = 'WebFace12M'
 
-filter_tool = FilterClass(attrARR_path, paths_path, columns, train_set)
+filter_tool = FilterClass(attrARR_path, paths_path, columns=columns_path, train_set=None)
 
 '''Examples of tuple inputs'''
-attr1 = ('Heavy_Makeup', 'abs', 0, 100)
+attr1 = ('Heavy_Makeup', 'rank', 50, 100)
 attr2 = ('vitor_gender', 0)
 attr_list = [attr1, attr2]
 
 new_paths = filter_tool.run(attr_list)
-print(len(new_paths))
 
-subset_tool = SubsetClass(new_paths, 10, 10, 10, 'loser face', False, attr_list)
+'''Create the subset tool and specify parameters for validation sets'''
+subset_tool = SubsetClass(new_paths, num_of_val_sets=10, num_of_matches=10000, num_of_non_matches=10000, file_name='Output File Name', replacement_bool=False, inputs=attr_list)
+
+'''Create the output file'''
 subset_tool.write_val_sets()
